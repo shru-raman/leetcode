c2a4d3ca342c4a381f256cb6477253e56414d37a
shru-raman <100361075+shru-raman@users.noreply.github.com>
2023-10-13 23:31:19 +0530

    Time: 2 ms (97.95%), Space: 40.7 MB (36.76%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for(int num : arr) {
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+        for(int num : map.values()){
+            boolean pres = set.add(num);
+            if(!pres) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
